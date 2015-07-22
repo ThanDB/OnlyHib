9790392c85e26ae0d051d415df5a7dbbbe1d194c
ThanDB <ThanK53Hus@gmail.com>
2015-07-22 21:11:19 +0700

    Lala

--- hib-tut12/src/main/java/com/thandb/tutorial/hibernate/chapter12/test/App.java
+++ hib-tut12/src/main/java/com/thandb/tutorial/hibernate/chapter12/test/App.java
@@ -3,6 +3,7 @@ package com.thandb.tutorial.hibernate.chapter12.test;
 import java.util.Date;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.thandb.tutorial.hibernate.chapter12.domain.Stock;
 import com.thandb.tutorial.hibernate.chapter12.domain.StockDailyRecord;
@@ -10,6 +11,107 @@ import com.thandb.tutorial.hibernate.chapter12.util.HibernateUtil;
 
 public class App {
 	public static void main(String[] args) {
+		/* BEGIN TEST FOR READ ONLY ENTITY */
+		Session session = null;
+		Transaction tx = null;
+		Integer id = new Integer(0);
+		try {
+			System.out.println("Start");
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			
+			// 1. Mutable in class
+			// 1.1 Mutable is true
+			/* Add
+			
+			Stock stock = new Stock();
+			stock.setStockCode("7277");
+			stock.setStockName("MICROSOFT");
+			Integer id = (Integer) session.save(stock);*/
+			
+			/* Update */
+			/*Stock stock = (Stock) session.get(Stock.class, new Integer(36));
+			stock.setStockName("Google");
+			session.saveOrUpdate(stock);
+			
+			tx.commit();
+			stock = (Stock) session.get(Stock.class, new Integer(36));
+			System.out.println("New Stock name: " + stock.getStockName());*/
+			
+			/* Delete */
+			/*Stock stock = (Stock) session.get(Stock.class, new Integer(36));
+			session.delete(stock);
+			
+			tx.commit();
+			stock = (Stock) session.get(Stock.class, new Integer(36));
+			if(stock == null) {
+				System.out.println("Stock is null");
+			} else {
+				System.out.println("Stock was not deleted. Stock name: " + stock.getStockName());
+			}*/
+			
+			// 1.2 Mutable is false
+			/* Add */
+			/*Stock stock = new Stock();
+			stock.setStockCode("7277");
+			stock.setStockName("MICROSOFT");
+			id = (Integer) session.save(stock);
+			tx.commit();
+			System.out.println("ID: " + id);
+			stock = (Stock) session.get(Stock.class, new Integer(id));
+			if(stock == null) {
+				System.out.println("Cannot add the stock.");
+			} else {
+				System.out.println("Added a new Stock: " + stock.getStockName());
+			}
+			System.out.println("Done");*/
+						
+			/* Update */
+			/*Stock stock = (Stock) session.get(Stock.class, new Integer(37));
+			stock.setStockName("Google");
+			session.saveOrUpdate(stock);
+
+			tx.commit();
+			stock = (Stock) session.get(Stock.class, new Integer(37));
+			if (stock == null) {
+				System.out.println();
+			} else {
+				if ("Google".equals(stock.getStockName())) {
+					System.out.println("Stock was not changed. Stock name: " + stock.getStockName());
+				} else {
+					System.out.println("Stock was changed. Stock name: " + stock.getStockName());
+				}
+			}*/
+						
+			/* Delete */
+			/*Stock stock = (Stock) session.get(Stock.class, new Integer(37));
+			session.delete(stock);
+
+			tx.commit();
+			stock = (Stock) session.get(Stock.class, new Integer(37));
+			if (stock == null) {
+				System.out.println("Stock was deleted.");
+			} else {
+				System.out.println("Stock was not deleted. Stock name: "
+						+ stock.getStockName());
+			}*/
+			
+			/*************************************************/
+			// 2.
+		} catch (Exception e) {
+			if(tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+		/* END TEST FOR READ ONLY ENTITY */
+		
+		/* BEGIN TEST FOR ONE TO MANY MAPPING */
+		/*
 		System.out.println("Hibernate one to many (XML Mapping)");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
@@ -34,5 +136,7 @@ public class App {
 
 		session.getTransaction().commit();
 		System.out.println("Done");
+		*/
+		/* END TEST FOR ONE TO MANY MAPPING */
 	}
 }
